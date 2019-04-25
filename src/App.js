85499ea26aef8bcbfc498b0d2299ef3acc1db7ed
Juuso Haapanen <juuso@haapanen.biz>
2019-04-25 19:48:21 +0300

    Changed example to use React state and useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-grid';
 import '@vaadin/vaadin-text-field';
 
-class App extends Component {
-  componentDidMount() {
-    let people = [];
-    this.refs.addButton.addEventListener('click', e => {
-      people = [
-        ...people,
-        {
-          firstName: this.refs.firstName.value,
-          lastName: this.refs.lastName.value
-        }
-      ];
-      this.refs.grid.items = people;
-      this.refs.firstName.value = '';
-      this.refs.lastName.value = '';
-    });
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="form">
-          <vaadin-text-field label="First Name" ref="firstName" />
-          <vaadin-text-field label="Last Name" ref="lastName" />
-          <vaadin-button ref="addButton"> Add </vaadin-button>
-        </div>
-        <vaadin-grid ref="grid">
-          <vaadin-grid-column path="firstName" header="First name" />
-          <vaadin-grid-column path="lastName" header="Last name" />
-        </vaadin-grid>
-      </div>
-    );
-  }
+
+const VaadinGrid = ({ content }) => {
+  useEffect(() => {
+    document.querySelector("#grid").items = content;
+  })
+  return (
+    <vaadin-grid id="grid">
+      <vaadin-grid-column path="firstName" header="First name" />
+      <vaadin-grid-column path="lastName" header="Last name" />
+    </vaadin-grid>
+  )
 }
+
+const App = () => {
+
+  const [people, setPerson] = useState([]);
+
+  useEffect(() => {
+    document.querySelector("#addButton").addEventListener('click', e => {
+      setPerson(people.concat({
+        firstName: document.querySelector("#firstName").value,
+        lastName: document.querySelector("#lastName").value
+      }));
+    });
+  })
+
+  return (
+    <>
+    <div className="form">
+      <vaadin-text-field label="First Name" id="firstName" />
+      <vaadin-text-field label="Last Name" id="lastName" />
+      <vaadin-button id="addButton"> Add </vaadin-button>
+    </div>
+    <VaadinGrid content={people} />
+    </>
+  )
+}
+
 
 export default App;
